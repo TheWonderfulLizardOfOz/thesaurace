e14491c8ad69a6a10db63f8e681fdaa6c27707d5
unknown <SXW1083@student.bham.ac.uk>
2025-03-22 13:15:29 +0000

    added synonym-getter function and theaurus saving function

--- gameTextPrototype.py
+++ gameTextPrototype.py
@@ -1,30 +1,40 @@
 import requests
 import json
+import time
 
 with open('cachedWords.json', 'r') as file:
     thesaurus = json.load(file)
 
 
+def sanitiser(word):
+    if not word.isalpha():
+        return False
+    return True
+
+def save_thesaurus():
+    with open('cachedWords.json', 'w', encoding='utf-8') as f:
+        json.dump(thesaurus, f, ensure_ascii=False, indent=4)
+
 def get_synonyms_of(word):
     if word in thesaurus.keys():
-        return thesaurus[keys]
+        return thesaurus[word]
     else:
-        return "Need to get those :P"
+        api_url = 'https://api.api-ninjas.com/v1/thesaurus?word={}'.format(word)
+        response = requests.get(api_url, headers={'X-Api-Key': 'Aqv8rZ0xlEOBQjnTf4cCrw==B6zBflzo1sc2Sz6D'})
+        if response.status_code == requests.codes.ok:
+            sanitised = list(filter(sanitiser,response.json()["synonyms"]))
+            thesaurus[word] = sanitised
+            save_thesaurus()
+            return thesaurus[word]
+        else:
+            print("Error:", response.status_code, response.text)
+            return -1
 
-print(get_synonyms_of("building"))
-print(get_synonyms_of("hello"))
 
-'''
+
+
+start = time.perf_counter()
+save_thesaurus()
+print(time.perf_counter()-start)
+
 word = 'foundations'
-api_url = 'https://api.api-ninjas.com/v1/thesaurus?word={}'.format(word)
-response = requests.get(api_url, headers={'X-Api-Key': 'Aqv8rZ0xlEOBQjnTf4cCrw==B6zBflzo1sc2Sz6D'})
-if response.status_code == requests.codes.ok:
-    print(response.text)
-    print(response.json()["synonyms"])
-    thesaurus[word] = response.json()["synonyms"]
-else:
-    print("Error:", response.status_code, response.text)
-
-with open('cachedWords.json', 'w', encoding='utf-8') as f:
-    json.dump(thesaurus, f, ensure_ascii=False, indent=4)
-'''
